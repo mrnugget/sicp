5eebf97a712f036128afb1d083fdd6067f26837b
Thorsten Ball <mrnugget@gmail.com>
2015-02-21 14:55:26 +0100

    Add ex 3.42

---
+++ ex3-42.rkt
@@ -0,0 +1,4 @@
+;; The solutions are basically the same, so both are valid.
+;; Bet Bitdiddle's solution doesn't change the behaviour from a concurrency
+;; standpoint. The only practical difference is that the same serializer is used
+;; every time (one for 'withdraw and one for 'deposit)
