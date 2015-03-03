9a8e2c04e976dcd999b88c5298ef13507ad3751a
Thorsten Ball <mrnugget@gmail.com>
2015-03-03 06:56:01 +0100

    Add ex 4.9

---
+++ ex4-9.rkt
@@ -0,0 +1,32 @@
+;; while loop
+(define test-exp '(while (> i 0) (display i)))
+
+;; can be rewritten as a derived expression like this
+
+(define (while-iter clause)
+  (if (clause) (begin
+                 (display i)
+                 (while-iter clause))))
+
+(define (while? exp)
+  (tagged-list? exp 'while))
+
+(define (while-condition exp)
+  (cadr exp))
+
+(define (while-body exp)
+  (caddr exp))
+
+(define (while->combination exp)
+  (list
+    (list 'define
+          (list 'while-iter)
+          (make-if (while-condition exp)
+                   (sequence->exp (list (while-body exp) (list 'while-iter)))
+                   '()))
+    (list 'while-iter)))
+
+;; To hook it up in eval:
+(while? exp) (eval (while->combination exp) env)
+
+(write (while->combination test-exp))
