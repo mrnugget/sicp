637d63d98df24d32819d36d3e58f07a13cb61ac6
Thorsten Ball <mrnugget@gmail.com>
2015-04-05 11:23:43 +0200

    Add 4.23

---
+++ ex4-23.rkt
@@ -0,0 +1,3 @@
+;; The version in ch4.1.7 unrolls the sequence into distinct lambdas.
+;; Alyssa's version will generate one lambda that repeatedly calls the
+;; procedures - when run. not in the analyze step.
