36135d8a24fec7bdb0330f34f9c61e5f5d0d8457
Thorsten Ball <mrnugget@gmail.com>
2015-04-26 15:19:16 +0200

    Add ex4.55

---
+++ ex4-55.rkt
@@ -0,0 +1,11 @@
+;; a) all the people supervised by Ben Bitdiddle
+
+(supervisor ?employee (Bitdiddle Ben))
+
+;; b) the names and jobs of all people in the accounting division
+
+(job ?name (accounting . ?job))
+
+;; c) the names and addresses of all people who live in Slumerville
+
+(address ?name (Slumerville . ?address-details)
