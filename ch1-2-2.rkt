0a4ff2b320c84e89f56d073be615b0362bdd6218
Thorsten Ball <mrnugget@gmail.com>
2015-05-06 14:23:56 +0200

    Add ch-1-2-2

---
+++ ch1-2-2.rkt
@@ -0,0 +1,17 @@
+#lang planet neil/sicp
+
+;; tree recursion
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+;; iterative recursion
+
+(define (fib n)
+  (define (fib-iter a b count)
+    (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+  (fib-iter 1 0 n))
