5580a8362580fb18c5a27562358d24c4f9bb27ad
Thorsten Ball <mrnugget@gmail.com>
2015-05-11 07:08:36 +0200

    Add examples and tests to ch5-2

--- ch5-2.rkt
+++ ch5-2.rkt
@@ -362,3 +362,66 @@
             (instruction-text inst) labels machine
             pc flag stack ops)))
       insts)))
+
+
+
+;;
+;; Testing the machine simulator
+;;
+
+(define gcd-machine
+  (make-machine
+    '(a b t)
+    (list (list 'rem remainder) (list '= =))
+    '(test-b
+       (test (op =) (reg b) (const 0))
+       (branch (label gcd-done))
+       (assign t (op rem) (reg a) (reg b))
+       (assign a (reg b))
+       (assign b (reg t))
+       (goto (label test-b))
+       gcd-done)))
+
+(set-register-contents! gcd-machine 'a 206)
+(set-register-contents! gcd-machine 'b 40)
+(start gcd-machine)
+(get-register-contents gcd-machine 'a)
+
+(define fib-machine
+  (make-machine
+    '(n continue val)
+    (list (list '< <) (list '- -) (list '+ +))
+    '((assign continue (label fib-done))
+      fib-loop
+      (test (op <) (reg n) (const 2))
+      (branch (label immediate-answer))
+      ;; set up to compute Fib(n - 1)
+      (save continue)
+      (assign continue (label afterfib-n-1))
+      (save n)                           ; save old value of n
+      (assign n (op -) (reg n) (const 1)); clobber n to n - 1
+      (goto (label fib-loop))            ; perform recursive call
+      afterfib-n-1                         ; upon return, val contains Fib(n - 1)
+      (restore n)
+      (restore continue)
+      ;; set up to compute Fib(n - 2)
+      (assign n (op -) (reg n) (const 2))
+      (save continue)
+      (assign continue (label afterfib-n-2))
+      (save val)                         ; save Fib(n - 1)
+      (goto (label fib-loop))
+      afterfib-n-2                         ; upon return, val contains Fib(n - 2)
+      (assign n (reg val))               ; n now contains Fib(n - 2)
+      (restore val)                      ; val now contains Fib(n - 1)
+      (restore continue)
+      (assign val                        ;  Fib(n - 1) +  Fib(n - 2)
+              (op +) (reg val) (reg n))
+      (goto (reg continue))              ; return to caller, answer is in val
+      immediate-answer
+      (assign val (reg n))               ; base case:  Fib(n) = n
+      (goto (reg continue))
+      fib-done)))
+
+(set-register-contents! fib-machine 'n 10)
+(start fib-machine)
+(get-register-contents fib-machine 'val)
